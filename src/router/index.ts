ed3eadeedcd7e07ef6230d9082d177fc7e0ee094
Sam Wong <samuelwong28@gmail.com>
2026-04-14 13:08:06 +0800

    update config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: HomePage,
+	},
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: routes,
 })
 
 export default router
