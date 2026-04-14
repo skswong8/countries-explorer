e14b8ecf1d2918ba58f2ca4890e90701d78476fd
Sam Wong <samuelwong28@gmail.com>
2026-04-14 23:00:56 +0800

    Connect to graphql countries api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+	uri: 'https://countries.trevorblades.com/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+	link: httpLink,
+	cache,
+})
 
 const app = createApp(App)
 
 app.use(router)
+
+app.provide(DefaultApolloClient, apolloClient)
 
 app.mount('#app')
