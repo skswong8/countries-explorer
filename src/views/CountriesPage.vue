acb9a632530ed7ee007f690a49e6f047f18d8992
Sam Wong <samuelwong28@gmail.com>
2026-04-16 00:05:01 +0800

    Reorder countries and output data

--- src/views/CountriesPage.vue
+++ src/views/CountriesPage.vue
@@ -3,9 +3,13 @@
 		<div v-if="loading">Loading countries...</div>
 		<div v-else-if="error">Error: {{ error.message }}</div>
 		<div v-else>
-			<p>{{ continent?.countries?.length || 0 }} countries in {{ continent?.name }}</p>
-			<div v-for="country in continent?.countries" :key="country.code">
-				{{ country.name }}
+			<h1>Countries in {{ continent?.name }}</h1>
+			<p>There are {{ continent?.countries?.length || 0 }} countries in {{ continent?.name }}</p>
+			<div class="country-cards">
+				<div class="country-card" v-for="country in countries" :key="country.code">
+					{{ country.emoji }}
+					{{ country.name }}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -49,4 +53,22 @@ const { result, loading, error } = useQuery<ContinentQuery>(GET_COUNTRIES_BY_CON
 })
 
 const continent = computed(() => result.value?.continent ?? null)
+// The API returns countries sorted by code. Reorder by name.
+const countries = computed(() => {
+	return [...(result.value?.continent?.countries || [])].sort((a, b) => {
+		return a.name.localeCompare(b.name)
+	})
+})
 </script>
+
+<style scoped>
+.country-cards {
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	gap: 1rem;
+}
+
+.country-card {
+	cursor: pointer;
+}
+</style>
